bb443efd42c1cf1c2564a1d24d82a1a4dfbcba30
DodginJam <157243206+DodginJam@users.noreply.github.com>
2025-10-07 18:26:05 +0100

    Moved logic for container interaction from the child BookContainer class to parent class ObjectContainer

--- Assets/Scripts/Books/BookContainer.cs
+++ Assets/Scripts/Books/BookContainer.cs
@@ -3,45 +3,6 @@ using UnityEngine;
 
 public class BookContainer : ObjectContainer<Book>
 {
-    public override void Interaction(Interactioner interactioner)
-    {
-        if (StoredGameObject == null)
-        {
-            Debug.Log("Container interacted with - it is empty");
-
-            if (interactioner.Inventory != null)
-            {
-                if (interactioner.Inventory.ObjectInHand != null)
-                {
-                    Debug.Log("Inventory hand has an object in it");
-
-                    if (ValidateObjectForContainer(interactioner.Inventory.ObjectInHand, out GameObject validGameObject))
-                    {
-                        interactioner.Inventory.TryRemoveObjectFromHand(true, false, Vector3.zero);
-
-                        SetObjectIntoContainer(validGameObject, true, false);
-                    }
-                }
-            }
-        }
-        else if (StoredGameObject != null)
-        {
-            Debug.Log("Container interacted with - it contains an object");
-
-            if (interactioner.Inventory != null)
-            {
-                if (interactioner.Inventory.ObjectInHand == null)
-                {
-                    Debug.Log("Inventory hand is empty");
-
-                    GameObject removedGameObject = RemoveObjectFromContainer(true, false);
-
-                    interactioner.Inventory.TryAddObjectToHand(removedGameObject);
-                }
-            }
-        }
-    }
-
     private void OnDrawGizmosSelected()
     {
         if (IsInterationAllowed)
--- Assets/Scripts/Books/ObjectContainer.cs
+++ Assets/Scripts/Books/ObjectContainer.cs
@@ -29,7 +29,44 @@ public abstract class ObjectContainer<T> : MonoBehaviour, IInteractable
     public GameObject StoredGameObject
     { get; private set; }
 
-    public abstract void Interaction(Interactioner interactioner);
+    public virtual void Interaction(Interactioner interactioner)
+    {
+        if (StoredGameObject == null)
+        {
+            Debug.Log("Container interacted with - it is empty");
+
+            if (interactioner.Inventory != null)
+            {
+                if (interactioner.Inventory.ObjectInHand != null)
+                {
+                    Debug.Log("Inventory hand has an object in it");
+
+                    if (ValidateObjectForContainer(interactioner.Inventory.ObjectInHand, out GameObject validGameObject))
+                    {
+                        interactioner.Inventory.TryRemoveObjectFromHand(true, false, Vector3.zero);
+
+                        SetObjectIntoContainer(validGameObject, true, false);
+                    }
+                }
+            }
+        }
+        else if (StoredGameObject != null)
+        {
+            Debug.Log("Container interacted with - it contains an object");
+
+            if (interactioner.Inventory != null)
+            {
+                if (interactioner.Inventory.ObjectInHand == null)
+                {
+                    Debug.Log("Inventory hand is empty");
+
+                    GameObject removedGameObject = RemoveObjectFromContainer(true, false);
+
+                    interactioner.Inventory.TryAddObjectToHand(removedGameObject);
+                }
+            }
+        }
+    }
 
     public bool ValidateObjectForContainer(GameObject objectToCheck, out GameObject validatedGameObject)
     {
