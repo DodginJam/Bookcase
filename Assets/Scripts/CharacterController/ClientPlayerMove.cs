8154c4b9a6f912bf1c7b6768474b7bfcb642666b
DodginJam <157243206+DodginJam@users.noreply.github.com>
2025-10-29 00:37:47 +0000

    Playing with predictive visual for mesh and camera movement on client prediction before server authority takesover

--- Assets/Scripts/CharacterController/ClientPlayerMove.cs
+++ Assets/Scripts/CharacterController/ClientPlayerMove.cs
@@ -12,6 +12,10 @@ public class ClientPlayerMove : NetworkBehaviour
     public PlayerController PlayerController
     { get; private set; }
 
+    [field: SerializeField]
+    public Transform PredictedVisual
+    { get; private set; }
+
     void Awake()
     {
         InputHandler.enabled = false;
@@ -50,13 +54,33 @@ public class ClientPlayerMove : NetworkBehaviour
 
     private void LateUpdate()
     {
-        // Frame sync.
-        if (!IsOwner)
+        if (IsOwner && !IsServer)
         {
-            return;
+            // Character Controller input movement prediction.
+            Vector3 predictedCharacterMovementVelocity = Vector3.zero;
+            Vector3 globalMovement = new Vector3(InputHandler.MovementInput.x, 0, InputHandler.MovementInput.y);
+            predictedCharacterMovementVelocity = PlayerController.MovementSpeed * Time.deltaTime * transform.TransformDirection(globalMovement);
+            PredictedVisual.transform.position += predictedCharacterMovementVelocity;
+
+            // Character Controller extenral forces / gravity movement prediction.
+            // Non-implementation yet.
+
+            // Character Transform Rotation Prediction.
+            if (InputHandler.RotationInput.x != 0)
+            {
+                // The player object should rotate on only the Y axis to allow change in X and Z movement direction.
+                Vector3 globalRotation = new Vector3(0, InputHandler.RotationInput.x, 0);
+
+                PredictedVisual.transform.Rotate(PlayerController.RotationSpeed * Time.deltaTime * globalRotation);
+            }
+
+            Debug.Log("Local movement");
         }
 
-        // If not the owner (i.e. the gameobject for the player), then send updates of the player input to the server.
-        UpdateInputServerRpc(InputHandler.MovementInput, InputHandler.RotationInput);
+        if (IsOwner)
+        {
+            // If not the owner (i.e. the gameobject for the player), then send updates of the player input to the server.
+            UpdateInputServerRpc(InputHandler.MovementInput, InputHandler.RotationInput);
+        }
     }
 }
